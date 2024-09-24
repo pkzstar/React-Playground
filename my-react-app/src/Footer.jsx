9cf0734447243e9261413d7f08979970941f563e
pkzstar <iceclimberelite@hotmail.com>
2024-09-24 13:27:08 -0500

    start

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+
+    return(
+        <footer>
+            <p>&copy; Your Wesbsite name</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header() {
+
+    return(
+        <header>
+            <h1> My Wesbsite</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Services</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+
+export default Header
