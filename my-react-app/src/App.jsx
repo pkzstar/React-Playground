09e68cc7095d78e705caf0994f2aa28cd19abb89
pkzstar <114553213+pkzstar@users.noreply.github.com>
2024-09-26 01:05:58 -0500

    .

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@
 
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx";
-// import Food from "./Food.jsx";
+import Food from "./Food.jsx";
 // import Card from "./Card.jsx";
 import Student from "./Student.jsx";
 import UserGreeting from "./UserGreeting.jsx";
@@ -45,7 +45,7 @@ function App() {
        </div>
        <OnChange />
        <hr></hr>
-       {/* <Food/> */}
+       <Food/>
        {/* <Card/> */}
        <MyComponent/>
        <ProfilePicture />
